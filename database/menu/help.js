3f628dc5902a358792ef53100926e3418c697f2c
MrDevils <54774508+adimas999@users.noreply.github.com>
2021-01-30 21:37:41 +0700

    Add files via upload

---
+++ database/menu/help.js
@@ -0,0 +1,126 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+╔════•ೋೋ•════╗ 
+              ${botName}
+╚════•ೋೋ•════╝
+
+╔════════════════════╗
+║║██░░░░░░░░░░░░░░░░░░╚╗╚╗
+║║██░░░░░Battery Low ░░░░░ ─║║║
+║║██░░░░░░░░░░░░░░░░░░╔╝╔╝
+╚════════════════════╝
+
+┏━━━⊱  *ABOUT USER*  ⊰━━┓
+┣⊱ *Nama* : ${pushname}
+┣⊱ *Nomer* : wa.me/${sender.split("@")[0]}
+┣⊱ *Uang mu* : ${uangku}
+┣⊱ *XP* : ${reqXp}
+┣⊱ *Prefix* : ${prefix}
+┣⊱ *Registered : ✔️
+┗━━━⊱  ⸨ *${botName}* ⸩  ⊰━━━━┛
+
+┏━━❉ *${botName}* ❉━━━┓
+┣⊱ *Creator* : *NazwaS*
+┣⊱ *Pengemban* : *MrDevils*
+┣⊱ *Whatsapp* : *85939888897*
+┣⊱ *Github* : github.com/adimas999
+┣⊱ *IG* : *adimas_sahadoet*
+┣⊱ *Owner*: *${ownerName}*
+┗━━━━━━━━━━━━━━━━
+
+┏━━❉ *INFO* ❉━━━┓
+┣⊱ *Dilarang spam bot*
+┣⊱ *Dilarang menelfon/call*
+┗━━━━━━━━━━━━━━━━
+
+┏━━⊱ *BOT MENU* ⊰━━┓
+┠≽ *${prefix}logomakermenu*
+┠≽ *${prefix}imagemakermenu*
+┠≽ *${prefix}stickermakermenu*
+┠≽ *${prefix}searchmenu*
+┠≽ *${prefix}educationmenu*
+┠≽ *${prefix}kerangmenu*
+┠≽ *${prefix}downloadermenu*
+┠≽ *${prefix}mememenu*
+┠≽ *${prefix}groupmenu*
+┠≽ *${prefix}soundmenu*
+┠≽ *${prefix}musicmenu*
+┠≽ *${prefix}islammenu*
+┠≽ *${prefix}stalkmenu*
+┠≽ *${prefix}wibumenu*
+┠≽ *${prefix}18+menu*
+┠≽ *${prefix}funmenu*
+┠≽ *${prefix}todmenu*
+┠≽ *${prefix}informationmenu*
+┠≽ *${prefix}stayonscreenmenu*
+┠≽ *${prefix}xpmenu*
+┠≽ *${prefix}limitmenu*
+┠≽ *${prefix}ownermenu*
+┠≽ *${prefix}othermenu*
+┣━━⊱  *${botName}*  ⊰━┫
+┃
+┃.                   // ¤ \\
+┃                    \\ ¤ //
+┃                       \\//
+┃                      (___)                    
+┃                      (___)
+┃                      (___)
+┃     \_____/\__/     \__/ \_____/
+┃       \ _ ​[     ​. *          ​ *.    ]​ _ /
+┃               \_  *          * _/
+┃                  . \ __°__ /.
+┃                      |\_°_/|
+┃                       [|\_/|]
+┃                       [|[¤]|]
+┃                       [|;¤;|]
+┃                       [;;¤;;]
+┃                      ;;;;¤]|]\
+┃                     ;;;;;¤]|]-\
+┃                    ;;;;;[¤]|]--\
+┃                   ;;;;;|[¤]|]---\
+┃                  ;;;;;[|[¤]|]|---|
+┃                 ;;;;;[|[¤]|]|---|
+┃                 ;;;;[|[¤]|/---/
+┃                 ;;;[|[¤]/---/
+┃                 ;;[|[¤/---/
+┃                   ;[|[/---/
+┃                   [|/---/
+┃                    /---/
+┃                  /---/|]
+┃                /---/]|];
+┃              /---/¤]|]
+┃             |---|[¤]|];;;
+┃             |---|[¤]|];;;;
+┃              \--|[¤]|];;;;     
+┃                \-|[¤]|];;;;
+┃                  \|[¤]|];;;
+┃                    \\¤//
+┃                      \|/
+┃
+┣━━⊱ *THANGKS TO* ⊰━┫
+┃
+┃█████████
+┃█▄█████▄█
+┃█▼▼▼▼▼
+┃█  *FADHIL GRAPHY*
+┃█  *Maslent*
+┃█  *XPTN(Agung)*
+┃█  *TobyG74*
+┃█  *VHTEAR*
+┃█  *MhankBarBar*
+┃█  *ArugaZ*
+┃█  *ZEKS*
+┃█  *MHRTZ*
+┃█  *AmpibiGaming*
+┃█  *YogaSakti*
+┃█  *BRYAN(SUHU)*
+┃█  *MrDevils*
+┃█  *NazwaS*
+┃█▲▲▲▲▲
+┃█████████
+┃ ██    ██
+┃
+┃
+┗━━⊱  ⸨ *${botName}* ⸩  ⊰━━━┛`
+}
+exports.help = help
